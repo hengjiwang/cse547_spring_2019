5f72e9437af6026ca0e6c1ca01ab23c14ed7b285
hengjiwang <hengjw@uw.edu>
2019-04-11 23:23:03 -0700

    hw0

---
+++ hw0/main.py
@@ -0,0 +1,13 @@
+import re
+import sys
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+lines = sc.textFile(sys.argv[1])
+words = lines.flatMap(lambda l: re.split(r'[^\w]+', l))
+alpha_words = words.filter(lambda w: w.isalpha()==True)
+pairs = alpha_words.map(lambda w: (w.lower()[0], 1))
+counts = pairs.reduceByKey(lambda n1, n2: n1 + n2)
+counts.saveAsTextFile(sys.argv[2])
+sc.stop()
